da77df3d9e2e22cd61e1ba1bdfc54029206f5163
jongough <jon.gough@eclipsesystems.com.au>
2020-06-16 17:43:35 +1000

    New script to test xml

---
+++ validate_xml.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$#" -ne "2" ]; then
+	echo "Incorrect invocation: Should be validate_xml.sh plugin_name plugin_version"
+	echo "where:"
+	echo "   plugin_name is the name of the plugin, i.e. testplugin_pi"
+	echo "   plugin_version is the version number, i.e. 1.0.114.0"
+	echo ""
+	echo "   Full command should look like:"
+	echo "      validate_xml.sh testplugin_pi 1.0.114.0"
+	exit
+fi
+
+for file in metadata/$1-$2*.xml
+do
+	`xmllint  --schema ocpn-plugins.xsd $file --noout 2> /dev/null`
+	rc=$?
+	if [ $rc -gt 0 ]; then
+		`xmllint  --schema ocpn-plugins.xsd $file --noout`
+	fi
+done
+
