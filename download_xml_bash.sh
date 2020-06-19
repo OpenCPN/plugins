9e2defdb0fd5494094e30be0273dcc688b3cbc78
jongough <jon.gough@eclipsesystems.com.au>
2020-06-20 08:27:15 +1000

    update to make more meaningful messages

--- download_xml_bash.sh
+++ download_xml_bash.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
 
-if [ "$#" -ne "4" ]; then
-	echo "Incorrect invocation: Should be download_xml.sh plugin_name plugin_version cloudsmith_user cloudsmith_level"
+if [ "$#" -ne "3" ]; then
+	echo "Incorrect invocation: Should be download_xml_bash.sh cloudsmith_repository plugin_version cloudsmith_user"
 	echo "where:"
-	echo "   plugin_name is the name of the plugin, i.e. testplugin_pi"
+	echo "   cloudsmith_repository is the name of the repository on cloudsmith the files are in, i.e. testplugin-prod"
 	echo "   plugin_version is the version number, i.e. 1.0.114.0"
-	echo "   cloudsmith_user is the user name associated with the cloudsmith repository, i.e. jon-gough"
-	echo "   cloudsmith_level is the level of the repository and is one of: prod, beta, alpha"
+	echo "   cloudsmith_user is the user name associated with the cloudsmith repository, i.e. jon-gough or opencpn"
 	echo ""
 	echo "   Full command should look like:"
-	echo "      download_xml.sh testplugin_pi 1.0.114.0 jon-gough alpha"
+	echo "      download_xml_bash.sh testplugin_pi-prod 1.0.114.0 jon-gough"
+	echo "      download_xml_bash.sh weather-routing-prod 1.13.8.0 opencpn"
 	exit
 fi
 
 REPO="https://cloudsmith.io/~$3/repos/"
-echo "Issuing command: wget -O - $REPO$1-$4/packages/?q=*$2*.xml |  awk {'print $2'} |grep '.xml$' |grep $1-$2"
+echo "Issuing command: wget -q -O - $REPO$1/packages/?q=*$2*.xml"
 my_array=()
 echo "Show current files that match criteria"
-ls metadata/$1-$2*xml -la
+ls metadata/$1*-$2*xml -la
 echo "Deleting current files that match criteria"
-rm metadata/$1-$2*xml
+rm metadata/$1*-$2*xml
 echo "Finding files on remote cloudsmith repository"
 delimiter="href=\""
 delimiter1=".xml\" title"
-s=$str$delimiter
 my_array=();
 while read -r line; do
 	if [[ $line == *$delimiter* ]] && [[ $line == *$delimiter1* ]]; then
@@ -33,9 +32,9 @@ while read -r line; do
 		end=$((end + 3 - start))
 		line=${line:$start:$end}
 		my_array+=( $line );
-		echo "line: $line"
+		echo "found: $line"
 	fi
-done < <(wget -O - "$REPO$1-$4/packages/?q=*$2*xml")
+done < <(wget -q -O - "$REPO$1/packages/?q=*$2*xml")
 
 echo "Downloading files found that match criteria"
 for URL in "${my_array[@]}"
@@ -44,5 +43,5 @@ do
   wget --progress=bar:force:noscroll -c $URL -P metadata
 done
 echo "Files downloaded"
-ls metadata/$1-$2*xml -la
+ls metadata/$1*-$2*xml -la
 
