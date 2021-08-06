e18cac14629c165a0fb3897b79ce72e883b28d3b
Alec Leamas <leamas.alec@nowhere.nt>
2021-08-06 08:53:01 +0200

    validate_xml.sh: cleanup, part 1.

--- validate_xml.sh
+++ validate_xml.sh
@@ -1,50 +1,50 @@
 #!/bin/bash
 
+function usage() {
+cat << EOT
+Usage:  validate_xml.sh <plugin_name> <version>
+
+Parameters:
+    plugin_name:
+        Name of plugin, for example testplugin_pi
+    version:
+        Complete version like 1.0.114.0
+
+Example:
+    validate_xml.sh testplugin_pi 1.0.114.0
+EOT
+}
+
+exit_rc=0
 if [ -z $CI ] && [ -z $GITHUB_ACTION ]; then
-    if [ "$#" -ne "2" ]; then
-        echo "Incorrect invocation: Should be validate_xml.sh plugin_name plugin_version"
-        echo "where:"
-        echo "   plugin_name is the name of the plugin, i.e. testplugin_pi"
-        echo "   plugin_version is the version number, i.e. 1.0.114.0"
-        echo ""
-        echo "   Full command should look like:"
-        echo "      validate_xml.sh testplugin_pi 1.0.114.0"
-        exit
-    fi
-    exit_rc=0
+    if [ "$#" -ne "2" ]; then usage; exit 0; fi
+
     for file in metadata/$1-$2*.xml
     do
-        `xmllint  --schema ocpn-plugins.xsd $file --noout 2> /dev/null`
-        rc=$?
-        if [ $rc -gt 0 ]; then
-            `xmllint  --schema ocpn-plugins.xsd $file --noout`
-            exit_rc=$rc
+        if ! xmllint --schema ocpn-plugins.xsd $file --noout 2>/dev/null
+        then
+            xmllint --schema ocpn-plugins.xsd $file --noout
+            exit_rc=1
         fi
     done
-    if [[ $exit_rc == 0 ]]; then
-        echo "All files pass xsd check"
-    fi
-    exit $exit_rc
 else
-    exit_rc=0
     URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${REQUEST_NO}/files"
-    FILES_FOUND=$(curl -s -X GET -G $URL | jq -r 'map(select(.status != "removed")) | .[] | .filename')
+    FILES_FOUND=$(curl -s -X GET -G $URL | \
+                  jq -r 'map(select(.status != "removed")) | .[] | .filename')
     files_array=($FILES_FOUND)
     for file in "${files_array[@]}"
     do
-        if [[ $file == "metadata"*".xml" ]]; then
+        if [[ $file == metadata*.xml ]]; then
             echo "Processing file: $file"
-            `xmllint  --schema ocpn-plugins.xsd $file --noout 2> /dev/null`
-            rc=$?
-            if [ $rc -gt 0 ]; then
-                `xmllint  --schema ocpn-plugins.xsd $file --noout`
-                exit_rc=$rc
+            if ! xmllint  --schema ocpn-plugins.xsd $file --noout 2> /dev/null
+            then
+                xmllint  --schema ocpn-plugins.xsd $file --noout
+                exit_rc=1
             fi
         fi
     done
-
-    if [[ $exit_rc == 0 ]]; then
-        echo "All files pass git pull xsd check"
-    fi
-    exit $exit_rc
 fi
+if [[ $exit_rc == 0 ]]; then
+    echo "All files pass xsd check"
+fi
+exit $exit_rc
