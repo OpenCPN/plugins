17fb3fecaa4aa191956641c304f9566213ce230e
Alec Leamas <leamas.alec@nowhere.nt>
2021-08-06 09:22:11 +0200

    validate_xml.sh: cleanup, part 2: Drop github and jq dependencies.
    Update the synopsis to reflect the actual code. Instead of pulling
    data using specific github urls and JSON parsing, take the diff
    directly using git. This means less dependencies and should be
    more robust.

--- validate_xml.sh
+++ validate_xml.sh
@@ -2,7 +2,7 @@
 
 function usage() {
 cat << EOT
-Usage:  validate_xml.sh <plugin_name> <version>
+Usage: validate_xml.sh [plugin_name version]
 
 Parameters:
     plugin_name:
@@ -10,39 +10,38 @@ Parameters:
     version:
         Complete version like 1.0.114.0
 
-Example:
+If invoked with plugin_name and version: validate all matching metadata
+using the .xsd file. Otherwise default to check the diff against
+upstream at https://github.com/OpenCPN/plugins using the same branch.
+
+Examples:
     validate_xml.sh testplugin_pi 1.0.114.0
+    validate_xml.sh
 EOT
 }
 
 exit_rc=0
-if [ -z $CI ] && [ -z $GITHUB_ACTION ]; then
-    if [ "$#" -ne "2" ]; then usage; exit 0; fi
-
-    for file in metadata/$1-$2*.xml
-    do
-        if ! xmllint --schema ocpn-plugins.xsd $file --noout 2>/dev/null
-        then
-            xmllint --schema ocpn-plugins.xsd $file --noout
-            exit_rc=1
-        fi
+if [ $# -eq 2 ]; then
+    for file in metadata/$1-$2*.xml; do
+        xmllint --schema ocpn-plugins.xsd $file --noout || exit_rc=1
     done
-else
-    URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${REQUEST_NO}/files"
-    FILES_FOUND=$(curl -s -X GET -G $URL | \
-                  jq -r 'map(select(.status != "removed")) | .[] | .filename')
-    files_array=($FILES_FOUND)
-    for file in "${files_array[@]}"
+elif [ $# -eq 0 ];  then
+    git remote add upstream https://github.com/OpenCPN/plugins.git
+    branch=$(git branch --show-current)
+    if ! git ls-remote --exit-code --heads upstream $branch; then
+        echo "Branch $branch can not be found on upstream, no checks done"
+        exit 0;
+    fi
+    git fetch upstream $branch
+    for f in $(git diff --name-only --diff-filter=ACMR upstream/$branch HEAD)
     do
         if [[ $file == metadata*.xml ]]; then
             echo "Processing file: $file"
-            if ! xmllint  --schema ocpn-plugins.xsd $file --noout 2> /dev/null
-            then
-                xmllint  --schema ocpn-plugins.xsd $file --noout
-                exit_rc=1
-            fi
+            xmllint --schema ocpn-plugins.xsd $file --noout || exit_rc=1
         fi
     done
+else
+    usage; exit 0
 fi
 if [[ $exit_rc == 0 ]]; then
     echo "All files pass xsd check"
