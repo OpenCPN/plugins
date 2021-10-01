89f02838f6fc59a7ee5540cdfc74514ab290854b
dsr <bdbcat@yahoo.com>
2021-09-30 20:22:31 -0400

    Revert "Update to latest testplugin"

---
+++ ci/ga_pull.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Validate all metadata which differs from upstream metadata at
+# https://github.com/OpenCPN/plugins.git. Requires a pull request
+# context where GITHUB_BASE_REF is defined.
+
+set -e
+
+sudo apt-get install libxml2-utils
+
+base_branch=$GITHUB_BASE_REF
+git remote add upstream https://github.com/OpenCPN/plugins.git
+git fetch upstream $base_branch
+
+if git diff --quiet upstream/$base_branch -- metadata; then
+    echo "No metadata changes, nothing to check."
+    exit 0
+fi
+
+exit_rc=0
+for f in $(git diff --name-only --diff-filter=ACMR upstream/$base_branch HEAD)
+do
+    if [[ "$f" == metadata*.xml ]]; then
+        echo "Processing file: $f"
+        xmllint --schema ocpn-plugins.xsd $f --noout || exit_rc=$((exit_rc | 1))
+        tools/check-metadata-urls $f || exit_rc=$((exit_rc | 2))
+    fi
+done
+
+if [ $exit_rc = 0 ];          then echo "All files pass checks" fi; fi
+if [ $((exit_rc & 1 )) = 1 ]; then echo "xmllint error(s)"; fi
+if [ $((exit_rc & 2 )) = 2 ]; then echo "Url access or checksum error(s)"; fi
+exit $exit_rc
--- validate_xml.sh
+++ validate_xml.sh
@@ -1,50 +1,49 @@
 #!/bin/bash
 
-if [ -z $CI ] && [ -z $GITHUB_ACTION ]; then
-    if [ "$#" -ne "2" ]; then
-        echo "Incorrect invocation: Should be validate_xml.sh plugin_name plugin_version"
-        echo "where:"
-        echo "   plugin_name is the name of the plugin, i.e. testplugin_pi"
-        echo "   plugin_version is the version number, i.e. 1.0.114.0"
-        echo ""
-        echo "   Full command should look like:"
-        echo "      validate_xml.sh testplugin_pi 1.0.114.0"
-        exit
-    fi
-    exit_rc=0
-    for file in metadata/$1-$2*.xml
-    do
-        `xmllint  --schema ocpn-plugins.xsd $file --noout 2> /dev/null`
-        rc=$?
-        if [ $rc -gt 0 ]; then
-            `xmllint  --schema ocpn-plugins.xsd $file --noout`
-            exit_rc=$rc
-        fi
-    done
-    if [[ $exit_rc == 0 ]]; then
-        echo "All files pass xsd check"
-    fi
-    exit $exit_rc
-else
-    exit_rc=0
-    URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${REQUEST_NO}/files"
-    FILES_FOUND=$(curl -s -X GET -G $URL | jq -r 'map(select(.status != "removed")) | .[] | .filename')
-    files_array=($FILES_FOUND)
-    for file in "${files_array[@]}"
-    do
-        if [[ $file == "metadata"*".xml" ]]; then
-            echo "Processing file: $file"
-            `xmllint  --schema ocpn-plugins.xsd $file --noout 2> /dev/null`
-            rc=$?
-            if [ $rc -gt 0 ]; then
-                `xmllint  --schema ocpn-plugins.xsd $file --noout`
-                exit_rc=$rc
-            fi
-        fi
-    done
+function usage() {
+cat << EOT
+Usage: validate_xml.sh [plugin_name version]
 
-    if [[ $exit_rc == 0 ]]; then
-        echo "All files pass git pull xsd check"
+Parameters:
+    plugin_name:
+        Name of plugin, for example testplugin_pi
+    version:
+        Complete version like 1.0.114.0
+
+If invoked with plugin_name and version: validate all matching metadata
+using the .xsd file. Otherwise default to check the diff against
+upstream at https://github.com/OpenCPN/plugins using the same branch.
+
+Examples:
+    validate_xml.sh testplugin_pi 1.0.114.0
+    validate_xml.sh
+EOT
+}
+
+exit_rc=0
+if [ $# -eq 2 ]; then
+    for file in metadata/$1-$2*.xml; do
+        xmllint --schema ocpn-plugins.xsd $file --noout || exit_rc=1
+    done
+elif [ $# -eq 0 ];  then
+    git remote add upstream https://github.com/OpenCPN/plugins.git
+    branch=$(git branch --show-current)
+    if ! git ls-remote --exit-code --heads upstream $branch; then
+        echo "Branch $branch can not be found on upstream, no checks done"
+        exit 0;
     fi
-    exit $exit_rc
+    git fetch upstream $branch
+    for f in $(git diff --name-only --diff-filter=ACMR upstream/$branch HEAD)
+    do
+        if [[ $file == metadata*.xml ]]; then
+            echo "Processing file: $file"
+            xmllint --schema ocpn-plugins.xsd $file --noout || exit_rc=1
+        fi
+    done
+else
+    usage; exit 0
 fi
+if [[ $exit_rc == 0 ]]; then
+    echo "All files pass xsd check"
+fi
+exit $exit_rc
